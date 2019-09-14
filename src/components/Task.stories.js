5551b949cfd5c125cd86c14d2f75d34880e3e25b
Sabine Robart <sabine.robart@decathlon.com>
2019-09-14 10:57:52 +0200

    Import withKnobs & object from knobs into the Task stories file

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
+import { withKnobs, object } from "@storybook/addon-knobs/react";
 
 import Task from "./Task";
 
