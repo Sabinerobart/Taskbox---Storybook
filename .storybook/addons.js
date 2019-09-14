c6678f704c235b10b900849d143857e8b2fde944
Sabine Robart <sabine.robart@decathlon.com>
2019-09-14 10:55:52 +0200

    Import the knobs addon

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,2 +1,7 @@
-import '@storybook/addon-actions/register';
-import '@storybook/addon-links/register';
+// 📝 Addon registration order matters!
+// The order you list these addons will dictate the order in which they appear
+// as tabs on your addon panel (for those that appear there).
+
+import "@storybook/addon-actions/register";
+import "@storybook/addon-knobs/register";
+import "@storybook/addon-links/register";
